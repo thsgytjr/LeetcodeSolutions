336ef73f127101e5b55d31e6909f2df542ab4b8c
David Son <sonda02@ca.com>
2018-08-20 18:36:44 -0500

    SprialMetric solution is add

--- src/RotateArray.java
+++ src/RotateArray.java
@@ -1,17 +1,26 @@
 public class RotateArray {
 
     void rotate(int[] nums, int k){
-        int[] result = new int[nums.length];
-        for (int i=0; i<k; i++){
-            result[i] = nums[nums.length-1];
+        k = k%nums.length;
+//        if (k==0) return;
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
+    }
 
+    void reverse(int[]nums, int start, int end){
+        int temp;
+        for (int i=start; i<=end;i++){
+            temp = nums[i];
+            nums[i] = nums[end];
+            nums[end] = temp;
+            end--;
         }
     }
 
     public static void main(String[] args) {
         int[] array = new int[]{1,2,3,4,5,6,7};
         RotateArray ra = new RotateArray();
-//        System.out.println(ra.rotate(array,3));
-
+        ra.rotate(array,2147483646);
     }
 }
