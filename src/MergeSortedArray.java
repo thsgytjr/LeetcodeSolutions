87b8b001ab11d85eba1c5817a42165c1ae355136
David Son <sonda02@ca.com>
2018-08-28 18:46:46 -0500

    Add DeepCopyLinkedList

--- src/MergeSortedArray.java
+++ src/MergeSortedArray.java
@@ -3,15 +3,22 @@ import java.util.Arrays;
 public class MergeSortedArray {
 
     public void merge(int[] nums1, int m, int[] nums2, int n){
-        for (int i=0; i<n; i++){
-            nums1[m]=nums2[i];
+        int[] result = new int[m+n];
+        for (int i=0; i<m;i++){
+            result[i] = nums1[i];
+        }
+        for (int i=0; i<n;i++){
+            result[m]=nums2[i];
             m++;
         }
-        Arrays.sort(nums1);
+        Arrays.sort(result);
+        for(int i=0; i<result.length;i++){
+            nums1[i]=result[i];
+        }
     }
 
     public static void main(String[] args) {
-        int[] nums1 = new int[]{1,2,3,0};
+        int[] nums1 = new int[]{1,2,3,0,0,0};
         int[] nums2 = new int[]{2};
         MergeSortedArray msa =new MergeSortedArray();
         msa.merge(nums1,3,nums2,1);
